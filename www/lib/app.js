044850e5af977b4e6f6b638e39f95d5cb2f71863
AraujoRodolfo <rodolfop.araujo@gmail.com>
2020-06-16 00:36:28 +0900

    Permite reiniciar o jogo quando chegar ao fim

--- www/lib/app.js
+++ www/lib/app.js
@@ -1,17 +1,26 @@
+//funcões
 function alertDismissed() {
     np++;
-    $('#pergunta').attr('src','img/'+perguntas[np]);    
-    $('#display').html('');
-    resposta = "";
+    if(np < perguntas.length){
+      $('#pergunta').attr('src','img/'+perguntas[np]);    
+      $('#display').html('');
+      resposta = "";
+    }else{
+      $('#pergunta').attr('src','img/fim.png');    
+      $('.alternativa').hide();
+      $('#display').html('Fim de Jogo');
+      $('#responder').html('Iniciar');      
+      np=-1; //volta para o inicio
+    }
 }
 
   var resposta = "";
   var perguntas = ['casa.png','carro.png'];
-  var certas = ['CASA','CARRO'];
   var np = 0;
 
+
   $(document).ready(function(){    
-    $('#pergunta').attr('src','img/'+perguntas[np]);    
+    $('#pergunta').attr('src','img/'+perguntas[np]);        
   });
 
   $(document).on('click','.alternativa',function(){
@@ -21,17 +30,30 @@ function alertDismissed() {
   });
 
   $(document).on('click','#responder',function(){
-    
-    if(resposta == certas[np]){            
-      navigator.notification.alert(
-          'Parabéns! Você acertou.',
-          alertDismissed, 
-          'Game', 
-          'Continuar'      
-      );
-    }else{
-      resposta ="";
-      navigator.vibrate(2000);
+    //caso o jogo tenha chego ao fim, aqui reinicia 
+    if(np == -1){
+      np = 0;
+      $('#pergunta').attr('src','img/'+perguntas[np]);   
       $('#display').html('');
+      $('#responder').html('Responder');       
+    }else{
+      //dividindo a string dos nomes das imagens
+      var res = perguntas[np].split(".");     
+      //pegando apenas o nome, sem a extensão e transformando em maiúsculo
+      res[0].toUpperCase();
+      
+      if(resposta == res[0].toUpperCase()){       
+        
+        navigator.notification.alert(
+            'Parabéns! Você acertou.',
+            alertDismissed, 
+            'Game', 
+            'Continuar'      
+        );
+      }else{
+        resposta ="";
+        navigator.vibrate(2000);
+        $('#display').html('');
+      }
     }
   });
