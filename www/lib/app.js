1706f05a66f82f28885d5b0174109675fa8820a2
AraujoRodolfo <rodolfop.araujo@gmail.com>
2020-06-15 23:15:57 +0900

    Alteração para uso com mais imagens..usando array

--- www/lib/app.js
+++ www/lib/app.js
@@ -1,25 +1,37 @@
-// This is a JavaScript file
-function alertDismissed() {}
+function alertDismissed() {
+    np++;
+    $('#pergunta').attr('src','img/'+perguntas[np]);    
+    $('#display').html('');
+    resposta = "";
+}
 
   var resposta = "";
-  
+  var perguntas = ['casa.png','carro.png'];
+  var certas = ['CASA','CARRO'];
+  var np = 0;
+
+  $(document).ready(function(){    
+    $('#pergunta').attr('src','img/'+perguntas[np]);    
+  });
 
   $(document).on('click','.alternativa',function(){
     resposta += $(this).html();
-    navigator.vibrate(500);
+    navigator.notification.beep(1);
     $('#display').html(resposta);
   });
 
   $(document).on('click','#responder',function(){
     
-    if(resposta == "CASA"){
+    if(resposta == certas[np]){            
       navigator.notification.alert(
           'Parabéns! Você acertou.',
           alertDismissed, 
           'Game', 
-          'Fechar'      
+          'Continuar'      
       );
     }else{
-      navigator.vibrate(3000);
+      resposta ="";
+      navigator.vibrate(2000);
+      $('#display').html('');
     }
   });
